cbb21d4a4c826528ecda6ac88a96668722fa8a87
blowtorch5 <pphilippot307@gmail.com>
2023-11-21 12:57:03 -0600

    Restored sql query for users

--- users.php
+++ users.php
@@ -12,6 +12,12 @@ require('connect.php');
 
 session_start();
 
+$query = "SELECT * FROM users ORDER BY username ASC";
+$statement = $db->prepare($query);
+$statement->execute();
+
+$users = $statement->fetchAll();
+
 ?>
 
 <!DOCTYPE html>
