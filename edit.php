3e27d0241c986f6eb1b4a1d5b8f6dc2b0a649e76
blowtorch5 <pphilippot307@gmail.com>
2023-11-21 13:04:46 -0600

    Quickly fixed edit page to show all category options

--- edit.php
+++ edit.php
@@ -71,6 +71,12 @@ if ($_POST && !empty($_POST['title']) && !empty($_POST['body'])){
     exit;
 }
 
+$query = "SELECT * FROM categories";
+$statement = $db->prepare($query);
+$statement->execute();
+
+$categories = $statement->fetchAll();
+
 ?>
 
 <!DOCTYPE html>
